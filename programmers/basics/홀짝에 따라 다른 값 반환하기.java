8814bc733a2668064f3d48fa532f0b79c79b058f
LimJaehun-H <ljhstar1222@gmail.com>
2026-05-21 14:17:07 +0900

    Create 홀짝에 따라 다른 값 반환하기.java

---
+++ programmers/basics/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,19 @@
+// 2026.05.21
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%2 !=0){
+            for (int i=1; i<=n; i=i+2){
+                answer = answer+i;
+            }
+            return answer;
+        }
+        else{
+            for (int i=0; i<=n; i=i+2){
+                answer = answer+i*i;
+            }
+            return answer;
+        }
+    }
+}
