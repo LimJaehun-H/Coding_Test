3ef181dcf16faf75c9e26d94e639cbf3aaba2111
LimJaehun-H <ljhstar1222@gmail.com>
2026-05-21 14:38:38 +0900

    Create 뒤에서 5등까지.java

---
+++ programmers/basics/뒤에서 5등까지.java
@@ -0,0 +1,10 @@
+// 2026.05.20
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        return Arrays.copyOfRange(num_list,0,5);
+    }
+}
