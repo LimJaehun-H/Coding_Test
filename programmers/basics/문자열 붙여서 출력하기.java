fce6bc4e82e477175b8de003b3f39a47c9c5d520
LimJaehun-H <ljhstar1222@gmail.com>
2026-05-21 14:06:24 +0900

    Create 문자열 붙여서 출력하기.java

---
+++ programmers/basics/문자열 붙여서 출력하기.java
@@ -0,0 +1,12 @@
+// 2026.05.21
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.printf("%s%s",a,b);
+    }
+}
